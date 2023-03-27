nodexeus/web

feat: added mqtt support

modules/client/grpc_client.ts
@@ -219,6 +219,8 @@ export interface StateObject {
   processNodeUpdate: (node: Node | undefined) => boolean;
 }
 
+const eqmx_url = 'ws://35.237.162.218/mqtt';
+
 export class GrpcClient {
   private authentication: AuthenticationServiceClient | undefined;
   private billing: BillingServiceClient | undefined;
@@ -1239,13 +1241,29 @@ export class GrpcClient {
   /* Update service */
 
   getUpdates(stateObject?: StateObject): void {
-    let client = mqtt.connect('mqtt://35.237.162.218:1883');
+    let mqtt_client = mqtt.connect(eqmx_url,
+        {
+          clean: true,
+          connectTimeout: 4000,
+          port: 8083,
+          protocolId: 'MQTT',
+          clientId: 'mqtt-js',
+          username: 'mqtt-js',
+          password: 'mqtt-js',
+        }
+    );
 
-    console.log('mqtt client: ', client);
+    mqtt_client.on('connect', function(err) {
+      console.log('MQTT connected');
+      mqtt_client.subscribe('js-test-topic', function (err) {
+        if (err) console.log("subscription error: ", err);
+        else console.log('MQTT subscribed to "js-test-topic"');
+      });
+    });
 
-    window.setTimeout(() => {
-      console.debug('Waiting 1000ms for next update');
-    }, 1000);
+    mqtt_client.on('error', function(err) {
+      console.log("MQTT connection error: ", err);
+    });
   }
 
   /* Command service */

blockjoy/blockvisor-app-web

modules/client/grpc_client.ts
@@ -251,7 +251,7 @@ export interface StateObject {
   processNodeUpdate: (node: Node | undefined) => boolean;
 }
 
-//const eqmx_url = 'ws://35.231.38.123/mqtt';
+const eqmx_url = 'ws://35.237.162.218/mqtt';
 
 export class GrpcClient {
   private authentication: AuthenticationServiceClient | undefined;
@@ -1306,52 +1306,28 @@ export class GrpcClient {
 
   /* Update service */
 
-  getUpdates(stateObject: StateObject): void {
-    let retry_count = 3;
-    let should_run = true;
-    let request_meta = new RequestMeta();
-    request_meta.setId(this.getDummyUuid());
+  getUpdates(stateObject?: StateObject): void {
+    let mqtt_client = mqtt.connect(eqmx_url, {
+      clean: true,
+      connectTimeout: 4000,
+      port: 8083,
+      protocolId: 'MQTT',
+      clientId: 'mqtt-js',
+      username: 'mqtt-js',
+      password: 'mqtt-js',
+    });
 
-    let request = new GetUpdatesRequest();
-    request.setMeta(request_meta);
-
-    let update_stream = this.update?.updates(request, this.getAuthHeader());
-
-    while (should_run) {
-      update_stream?.on('data', (response) => {
-        if (
-          response.getUpdate()?.getNotificationCase() ===
-          UpdateNotification.NotificationCase.HOST
-        ) {
-          const host = response.getUpdate()?.getHost();
-
-          console.log(`got host update from server: `, host);
-          stateObject.processHostUpdate(host);
-        } else if (
-          response.getUpdate()?.getNotificationCase() ===
-          UpdateNotification.NotificationCase.NODE
-        ) {
-          const node = response.getUpdate()?.getNode();
-
-          console.log(`got node update from server: `, node);
-          stateObject.processNodeUpdate(node);
-        }
+    mqtt_client.on('connect', function (err) {
+      console.log('MQTT connected');
+      mqtt_client.subscribe('js-test-topic', function (err) {
+        if (err) console.log('subscription error: ', err);
+        else console.log('MQTT subscribed to "js-test-topic"');
       });
-      update_stream?.on('error', (err) => {
-        console.error(`update stream closed unexpectedly: `, err);
-        if (retry_count > 0) {
-          console.info('Trying to reinitialize the update connection');
-          update_stream = this.update?.updates(request, this.getAuthHeader());
-          retry_count--;
-        } else {
-          should_run = false;
-        }
-      });
+    });
 
-      window.setTimeout(() => {
-        console.debug('Waiting 1000ms for next update');
-      }, 1000);
-    }
+    mqtt_client.on('error', function (err) {
+      console.log('MQTT connection error: ', err);
+    });
   }
 
   /* Command service */