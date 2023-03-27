nodexeus/web

feat: added mqtt support

modules/client/grpc_client.ts
@@ -219,6 +219,8 @@ export interface StateObject {
   processNodeUpdate: (node: Node | undefined) => boolean;
 }
 
+const eqmx_url = 'ws://35.237.162.218/mqtt';
+
 export class GrpcClient {
   private authentication: AuthenticationServiceClient | undefined;
   private billing: BillingServiceClient | undefined;
@@ -1239,13 +1241,29 @@ export class GrpcClient {
   /* Update service */
 
   getUpdates(stateObject?: StateObject): void {
-    let client = mqtt.connect('mqtt://35.237.162.218:1883');
+    let mqtt_client = mqtt.connect(eqmx_url,
+        {
+          clean: true,
+          connectTimeout: 4000,
+          port: 8083,
+          protocolId: 'MQTT',
+          clientId: 'mqtt-js',
+          username: 'mqtt-js',
+          password: 'mqtt-js',
+        }
+    );
 
-    console.log('mqtt client: ', client);
+    mqtt_client.on('connect', function(err) {
+      console.log('MQTT connected');
+      mqtt_client.subscribe('js-test-topic', function (err) {
+        if (err) console.log("subscription error: ", err);
+        else console.log('MQTT subscribed to "js-test-topic"');
+      });
+    });
 
-    window.setTimeout(() => {
-      console.debug('Waiting 1000ms for next update');
-    }, 1000);
+    mqtt_client.on('error', function(err) {
+      console.log("MQTT connection error: ", err);
+    });
   }
 
   /* Command service */

blockjoy/blockvisor-app-web

modules/client/grpc_client.ts
@@ -1305,13 +1305,27 @@ export class GrpcClient {
   /* Update service */
 
   getUpdates(stateObject?: StateObject): void {
-    let client = mqtt.connect('mqtt://35.237.162.218:1883');
+    let mqtt_client = mqtt.connect(eqmx_url, {
+      clean: true,
+      connectTimeout: 4000,
+      port: 8083,
+      protocolId: 'MQTT',
+      clientId: 'mqtt-js',
+      username: 'mqtt-js',
+      password: 'mqtt-js',
+    });
 
-    console.log('mqtt client: ', client);
+    mqtt_client.on('connect', function (err) {
+      console.log('MQTT connected');
+      mqtt_client.subscribe('js-test-topic', function (err) {
+        if (err) console.log('subscription error: ', err);
+        else console.log('MQTT subscribed to "js-test-topic"');
+      });
+    });
 
-    window.setTimeout(() => {
-      console.debug('Waiting 1000ms for next update');
-    }, 1000);
+    mqtt_client.on('error', function (err) {
+      console.log('MQTT connection error: ', err);
+    });
   }
 
   /* Command service */