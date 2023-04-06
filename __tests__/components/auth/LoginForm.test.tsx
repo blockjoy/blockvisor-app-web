2275278700d7fad6afdd2f776be30519884e7759
Bojan Bušljeta <bojan@prototyp.digital>
2023-04-06 11:04:45 +0200

    fix user events

--- __tests__/components/auth/LoginForm.test.tsx
+++ __tests__/components/auth/LoginForm.test.tsx
@@ -1,10 +1,22 @@
-import { expect, vi, it, describe, afterEach } from 'vitest';
-import { cleanup, fireEvent, render, screen } from '../../renderer';
+import { expect, vi, it, describe, afterEach, beforeEach } from 'vitest';
+import {
+  cleanup,
+  fireEvent,
+  render,
+  screen,
+  act,
+  waitFor,
+} from '../../renderer';
 import { LoginForm } from '@modules/auth';
 import { useRouterSpy } from '../../utils';
 import { routerMockBuilder } from '__tests__/mocks/router';
+import { useGetOrganizations } from '@modules/organization';
+import userEvent from '@testing-library/user-event';
 
 describe('Login Form', () => {
+  beforeEach(() => {
+    vi.mock('@modules/organization/hooks/useGetOrganizations');
+  });
   afterEach(() => {
     cleanup();
   });
@@ -14,6 +26,15 @@ describe('Login Form', () => {
   );
 
   it('should have submit button disabled by default', () => {
+    vi.mocked(useGetOrganizations).mockReturnValue({
+      getOrganizations: vi.fn(),
+      organizations: [],
+      total: 0,
+      removeFromOrganizations: vi.fn(),
+      addToOrganizations: vi.fn(),
+      isLoading: 'finished',
+      setIsLoading: vi.fn(),
+    });
     render(<LoginForm />);
 
     const submitButton = screen.getByDataCy('login-submit-button');
@@ -22,23 +43,27 @@ describe('Login Form', () => {
   });
 
   it('should have submit button enabled when vaild data is entered', () => {
+    vi.mocked(useGetOrganizations).mockReturnValue({
+      getOrganizations: vi.fn(),
+      organizations: [],
+      total: 0,
+      removeFromOrganizations: vi.fn(),
+      addToOrganizations: vi.fn(),
+      isLoading: 'finished',
+      setIsLoading: vi.fn(),
+    });
+
     render(<LoginForm />);
 
     const emailInput = screen.getByDataCy('login-email-input');
-
-    fireEvent.change(emailInput, {
-      target: { value: 'test@test.com' },
-    });
-
     const passwordInput = screen.getByDataCy('login-password-input');
 
-    fireEvent.change(passwordInput, {
-      target: { value: 'test123' },
+    userEvent.type(emailInput, 'test@test.com');
+    userEvent.type(passwordInput, 'test@test.com');
+
+    waitFor(() => {
+      const submitButton = screen.getByDataCy('login-submit-button');
+      expect(submitButton).not.toHaveProperty('disabled');
     });
-
-    const submitButton = screen.getByDataCy('login-submit-button');
-
-    fireEvent.click(submitButton);
-    expect(submitButton).not.toHaveProperty('disabled');
   });
 });
