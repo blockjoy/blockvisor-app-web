a31d53ec6a16cb7afa536572aa4865e98cd6bcc6
Bojan Bušljeta <bojan@prototyp.digital>
2023-04-06 11:04:45 +0200

    fix user events

--- __tests__/components/auth/ForgotPasswordForm.test.tsx
+++ __tests__/components/auth/ForgotPasswordForm.test.tsx
@@ -1,15 +1,16 @@
-import { expect, vi, it, describe, afterEach } from 'vitest';
-import { cleanup, fireEvent, render, screen } from '../../renderer';
-import { ForgotPasswordForm, LoginForm } from '@modules/auth';
+import { expect, it, describe, afterEach } from 'vitest';
+import { cleanup, render, screen, waitFor } from '../../renderer';
+import { ForgotPasswordForm } from '@modules/auth';
 import { useRouterSpy } from '../../utils';
 import { routerMockBuilder } from '__tests__/mocks/router';
+import userEvent from '@testing-library/user-event';
 
 afterEach(() => {
   cleanup();
 });
 describe('Forgot Password Form', () => {
   useRouterSpy.mockImplementation(() =>
-    routerMockBuilder({ pathname: '/login?redirect=%2F' }),
+    routerMockBuilder({ pathname: '/forgot-password' }),
   );
 
   it('should have submit button disabled by default', () => {
@@ -22,17 +23,19 @@ describe('Forgot Password Form', () => {
     expect(submitButton.disabled).toEqual(true);
   });
 
-  /* it('should have submit button enabled when vaild data is entered', () => {
-    const { rerender } = render(<ForgotPasswordForm />);
+  it('should have submit button enabled when vaild data is entered', () => {
+    render(<ForgotPasswordForm />);
 
-    fireEvent.change(screen.getByDataCy('forgot-password-email-input'), {
-      target: { value: 'test@test.com' },
+    userEvent.type(
+      screen.getByDataCy('forgot-password-email-input'),
+      'test@test.com',
+    );
+
+    waitFor(() => {
+      const submitButton = screen.getByDataCy(
+        'forgot-password-submit',
+      ) as HTMLButtonElement;
+      expect(submitButton.disabled).toEqual(false);
     });
-
-    const submitButton = screen.getByDataCy(
-      'forgot-password-submit',
-    ) as HTMLButtonElement;
-
-    expect(submitButton.disabled).toEqual(false);
-  }); */
+  });
 });
