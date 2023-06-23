99f7862dfd0d7613d1ed8063e203bb2bf70c6738
Dragan Rakita <draganrakita6@gmail.com>
2023-06-23 14:52:45 +0200

    feat: sc-410 default billing address when adding payment source

--- shared/components/Forms/VanillaForm/Checkbox/Checkbox.tsx
+++ shared/components/Forms/VanillaForm/Checkbox/Checkbox.tsx
@@ -1,10 +1,4 @@
-import {
-  ChangeEvent,
-  ChangeEventHandler,
-  InputHTMLAttributes,
-  MouseEventHandler,
-  ReactNode,
-} from 'react';
+import { ChangeEventHandler, ReactNode } from 'react';
 import { display } from 'styles/utils.display.styles';
 import { styles } from './Checkbox.styles';
 
@@ -15,6 +9,7 @@ type Props = {
   description?: string;
   formTouched?: boolean;
   checked?: boolean;
+  disabled?: boolean;
   onChange: ChangeEventHandler<HTMLInputElement>;
   children?: ReactNode;
 };
@@ -24,6 +19,7 @@ export function Checkbox({
   id,
   name,
   formTouched = false,
+  disabled = false,
   checked,
   onChange,
   children,
@@ -36,12 +32,20 @@ export function Checkbox({
         css={[display.visuallyHidden]}
         id={id}
         name={name}
+        disabled={disabled}
         defaultChecked={checked}
         type="checkbox"
         {...rest}
         onChange={onChange}
       />
-      <label css={[styles.base, checked ? styles.input : '']} htmlFor={id}>
+      <label
+        css={[
+          styles.base,
+          checked ? styles.input : '',
+          disabled ? styles.disabled : '',
+        ]}
+        htmlFor={id}
+      >
         {children}
       </label>
     </>
