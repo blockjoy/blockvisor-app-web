e3fdc0980c4621de5a73db8f1ebe5486e0c0ec2f
Dragan Rakita <draganrakita6@gmail.com>
2023-07-10 13:01:29 +0200

    fix: sc-2031 permission to create a new host

--- modules/layout/components/AppLayout.tsx
+++ modules/layout/components/AppLayout.tsx
@@ -83,10 +83,6 @@ export const AppLayout = ({ children, isPageFlex, pageTitle }: LayoutProps) => {
     }
   }, [defaultOrganization?.id]);
 
-  useEffect(() => {
-    getSubscription(defaultOrganization?.id!);
-  }, [defaultOrganization?.id]);
-
   return (
     <>
       <Head>
