01278b414bc373f8bf0eec3b6666c573d99fa4bd
StjepanDzojic <stjepan@prototyp.digital>
2023-08-07 16:08:30 +0200

    Added mocked data rpc requests data

--- modules/node/components/NodeView/SidePanel/NodeViewSidePanel.tsx
+++ modules/node/components/NodeView/SidePanel/NodeViewSidePanel.tsx
@@ -4,6 +4,8 @@ import { styles } from './NodeViewSidePanel.styles';
 
 export const NodeViewSidePanel = () => {
   const { node } = useNodeView();
+
+  //TODO: add real data
   return (
     <>
       <div css={styles.form}>
@@ -16,11 +18,13 @@ export const NodeViewSidePanel = () => {
           ) : (
             <div css={styles.blockheightLoader}>
               <NodeStatusIcon size="16px" status={1} />
-              <p css={styles.blockheightLoaderText}>Syncing</p>
+              <p css={styles.blockheightLoaderText}>Syancing</p>
             </div>
           )}
         </div>
       </div>
+      <h3 css={styles.formHeader}>RPC Requests</h3>
+      <span css={styles.formValue}>112.3k p/s</span>
       {node?.blockHeight! > -1 && (
         <NetdataDashboard nodeId={node?.id!} isSidePanel />
       )}
