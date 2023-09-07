de84934190951ac46b6b7397e5e7077875a9c9da
Dragan Rakita <draganrakita6@gmail.com>
2023-09-07 18:16:00 +0200

    feat: [sc-2713] protected route hoc

---
+++ modules/auth/components/ProtectedRoute/ProtectedRoute.tsx
@@ -0,0 +1,24 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { useIdentity } from '@modules/auth';
+import { LoadingSpinner } from '@shared/components';
+import { ROUTES } from '@shared/constants/routes';
+
+interface Props {
+  children?: any;
+}
+
+export function ProtectedRoute({ children }: Props) {
+  const router = useRouter();
+  const { isSuperUser } = useIdentity();
+
+  useEffect(() => {
+    if (!isSuperUser) {
+      router.push(ROUTES.DEFAULT, undefined, {
+        shallow: true,
+      });
+    }
+  }, [router.isReady, isSuperUser]);
+
+  return !isSuperUser ? <LoadingSpinner size="page" /> : children;
+}
