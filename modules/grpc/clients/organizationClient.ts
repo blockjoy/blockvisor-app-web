1e7a4b5f3b9d91ec1eaa01d6ca7a805f07ee641f
Dragan Rakita <draganrakita6@gmail.com>
2023-10-11 09:55:03 +0200

    fix: ruined branch due to api changes

--- modules/grpc/clients/organizationClient.ts
+++ modules/grpc/clients/organizationClient.ts
@@ -43,6 +43,7 @@ class OrganizationClient {
     }
   }
 
+<<<<<<< HEAD
   async getOrganizations(
     pagination?: UIPagination,
     keyword?: string,
@@ -64,12 +65,20 @@ class OrganizationClient {
       request.search = search;
     }
 
+=======
+  async getOrganizations(): Promise<Org[] | StatusResponse> {
+    const request = { memberId: getIdentity().id, offset: 0, limit: 100 };
+>>>>>>> f4bccfe2 (fix: ruined branch due to api changes)
     console.log('listOrganizationsRequest', request);
     try {
       await authClient.refreshToken();
       const response = await this.client.list(request, getOptions());
       console.log('listOrganizationsResponse', response);
+<<<<<<< HEAD
       return response;
+=======
+      return response.orgs;
+>>>>>>> f4bccfe2 (fix: ruined branch due to api changes)
     } catch (err: any) {
       return handleError(err);
     }
