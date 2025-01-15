nodexeus/web

adding host costs to admin panel

modules/admin/components/AdminLists/AdminNodes/AdminNodes.tsx
@@ -1,6 +1,6 @@
 import { AdminList } from '../AdminList/AdminList';
 import { blockchainClient, nodeClient } from '@modules/grpc';
-import { DateTime, NodeStatus } from '@shared/components';
+import { Currency, DateTime, NodeStatus } from '@shared/components';
 import {
   AdminNodesFilterBlockchain,
   AdminNodesFilterOrg,
@@ -70,6 +70,10 @@ const columns: AdminListColumn[] = [
     isVisible: false,
     filterComponent: AdminNodesSyncStatus,
     filterDropdownMinWidth: 120,
+  },
+  {
+    name: 'cost',
+    width: '100px',
   },
   {
     name: 'host',
@@ -219,6 +223,7 @@ export const AdminNodes = () => {
         createdAt: <DateTime date={node.createdAt!} />,
         createdBy: node.createdBy?.name,
         host: node.hostName,
+        cost: <Currency cents={node.cost?.amount?.amountMinorUnits!} />,
       };
     });
 

blockjoy/blockvisor-app-web

modules/admin/components/AdminLists/AdminNodes/AdminNodes.tsx
@@ -1,6 +1,6 @@
 import { AdminList } from '../AdminList/AdminList';
 import { protocolClient, nodeClient } from '@modules/grpc';
-import { DateTime, NodeStatus } from '@shared/components';
+import { DateTime, NodeStatus, Currency } from '@shared/components';
 import {
   AdminNodesFilterProtocol,
   AdminNodesFilterOrg,
@@ -201,21 +201,11 @@ export const AdminNodes = () => {
         nodeState: (
           <NodeStatus status={node.nodeStatus?.state!} hasBorder={false} />
         ),
-        // containerStatus: (
-        //   <NodeStatus
-        //     status={node.containerStatus}
-        //     type="container"
-        //     hasBorder={false}
-        //   />
-        // ),
-        // syncStatus: (
-        //   <NodeStatus status={node.syncStatus} type="sync" hasBorder={false} />
-        // ),
-        // nodeType: capitalized(convertNodeTypeToName(node.nodeType)),
         region: node.placement?.scheduler?.region,
         createdAt: <DateTime date={node.createdAt!} />,
         // createdBy: node.createdBy?.resourceId,
         host: node.hostDisplayName || node.hostNetworkName,
+        cost: <Currency cents={node.cost?.amount?.amountMinorUnits!} />,
       };
     });
 